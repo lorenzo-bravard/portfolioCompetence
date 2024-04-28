82d5d9043c97e9baba64962b9399db3680f20c69
lorenzo-bravard <lorenzo.bravard@gmail.com>
2024-04-28 22:17:26 +0200

    initialisation

---
+++ components/navigation/NavItem.tsx
@@ -0,0 +1,11 @@
+import { TestIcon } from "@/app/_compnents/icons/TestIcon"
+
+export const NavItem = () => {
+    return (
+        
+        <div>
+        <TestIcon size={54} className="text-foreground"/>
+        <p>Test</p>
+        </div>
+    )
+}
---
+++ components/navigation/Navigation.tsx
@@ -0,0 +1,10 @@
+import { NavItem } from "@/components/navigation/NavItem"
+
+
+export const Navigation = () => {
+    return (
+        <nav className="border-solid border-r-2 border-e5e7eb-500 w-20 h-full fixed">
+            <NavItem></NavItem>
+        </nav>
+    )
+}
